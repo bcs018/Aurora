a1fe130a3a96c33e50ac0c9e82d4d0e462583957
Bruno César da Silva <57082742+bcs018@users.noreply.github.com>
2024-11-14 15:33:59 -0300

    Colocar nome do usuário logado no menu

--- aurora/resources/views/components/site/header.blade.php
+++ aurora/resources/views/components/site/header.blade.php
@@ -14,12 +14,15 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{route('home.indexPage')}}">{{ env('APP_NAME') }}</a>
+            <a class="navbar-brand" href="{{route('home.indexPage')}}">{{ env('APP_NAME') }}</a> 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <span class="nav-link" style="color: #666565">{{Auth::user()->name}} &nbsp;&nbsp;| </span>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('home.indexPage')}}">Home</a>
                     </li>
