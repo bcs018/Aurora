7cd4ebe600e92610e3c3f7ef09a26120e67b37a7
Bruno César da Silva <57082742+bcs018@users.noreply.github.com>
2025-08-05 17:28:39 -0300

    Alterado a versão

--- aurora/resources/views/components/painel/header.blade.php
+++ aurora/resources/views/components/painel/header.blade.php
@@ -119,7 +119,7 @@
         </div>
         <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-                <b>Versão</b> 1.0.2
+                <b>Versão</b> 1.1.0
             </div>
             <strong>A.R.L.S. AURORA GUAÇUANA
         </footer>
